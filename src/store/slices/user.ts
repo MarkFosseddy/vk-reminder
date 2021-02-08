dffbeca2896e76580bbf0aac4d4e666bf26f5634
Fosseddy <MarkFosseddy@gmail.com>
2021-02-08 17:39:37 +0500

    user slice small refactor

--- src/store/slices/user.ts
+++ src/store/slices/user.ts
@@ -35,105 +35,99 @@ export const userSlice = createSlice({
     setLoading(state, action: PayloadAction<boolean>) {
       state.loading = action.payload;
     },
-    // @TODO: clean up
     setInitLoading(state, action: PayloadAction<boolean>) {
       state.initLoading = action.payload;
     },
     setError(state, action: PayloadAction<string | null>) {
       state.error = action.payload;
     },
-    loginStart(state) {
-      state.loading = true;
-      state.error = null;
-    },
-    loginSuccess(state, action: PayloadAction<User>) {
-      state.loading = false;
+    setUser(state, action: PayloadAction<User | null>) {
       state.user = action.payload;
-    },
-    loginFail(state) {
-      state.loading = false;
-      state.error = "Something went wrong :(";
-    },
-    resetState() {
-      return initialState;
     }
   }
 });
 
-export const loginAction = (history: History): StoreThunk => async dispatch => {
-  const { loginStart, loginFail, loginSuccess } = userSlice.actions;
+export function loginAction(history: History): StoreThunk {
+  return async function(dispatch) {
+    const { setLoading, setError, setUser } = userSlice.actions;
 
-  const loginRes = await VKLib.Auth.login();
-  if (!loginRes.session) return;
+    const loginRes = await VKLib.Auth.login();
+    if (!loginRes.session) return;
 
-  dispatch(loginStart());
-  // @TODO: delete this
-  await new Promise(resolve => setTimeout(() => { resolve(true); }, 1000));
-  const userInfoRes = await VKLib.Api.getUserInfo(loginRes.session.user.id);
+    dispatch(setLoading(true));
+    dispatch(setError(null));
+    
+    // @TODO: delete this
+    await new Promise(resolve => setTimeout(() => { resolve(true); }, 1000));
 
-  if (userInfoRes.error || !userInfoRes.response[0]) {
-    dispatch(loginFail());
-    return;
-  }
+    const userInfoRes = await VKLib.Api.getUserInfo(loginRes.session.user.id);
 
-  const { id, last_name, first_name, photo_100 } = userInfoRes.response[0];
-  localStorage.setItem(StorageKeys.VK_ID, id);
-  dispatch(loginSuccess({ id, last_name, first_name, photo_100 }));
+    if (userInfoRes.error || !userInfoRes.response[0]) {
+      dispatch(setError("Something went wrong :("));
+      dispatch(setLoading(false));
+      return;
+    }
 
-  history.replace("/allow-community-messages");
-};
+    const { id, last_name, first_name, photo_100 } = userInfoRes.response[0];
+    localStorage.setItem(StorageKeys.VK_ID, id);
+    dispatch(setUser({ id, last_name, first_name, photo_100 }));
+    dispatch(setLoading(false));
 
-export const persistLoginAction = (
-  history: History,
-  redirectPath: string
-): StoreThunk => async dispatch => {
-  const { loginSuccess, setInitLoading } = userSlice.actions;
+    history.replace("/allow-community-messages");
+  };
+}
 
-  // @TODO: clean up
-  dispatch(setInitLoading(true));
+export function persistLoginAction(history: History, redirectPath: string): StoreThunk {
+  return async function(dispatch) { 
+    const { setInitLoading, setUser } = userSlice.actions;
 
-  // @TODO: delete this
-  await new Promise(resolve => setTimeout(() => resolve(true), 500));
-  const loginStatusRes = await VKLib.Auth.getLoginStatus();
-  const userId = localStorage.getItem(StorageKeys.VK_ID);
+    dispatch(setInitLoading(true));
 
-  if (!loginStatusRes.session || !userId) {
-    // @TODO: clean up
+    // @TODO: delete this
+    await new Promise(resolve => setTimeout(() => resolve(true), 500));
+
+    const loginStatusRes = await VKLib.Auth.getLoginStatus();
+    const userId = localStorage.getItem(StorageKeys.VK_ID);
+
+    if (!loginStatusRes.session || !userId) {
+      dispatch(setInitLoading(false));
+      return;
+    }
+
+    // @TODO: delete this
+    await new Promise(resolve => setTimeout(() => resolve(true), 1000));
+
+    const userInfoRes = await VKLib.Api.getUserInfo(userId);
+
+    if (userInfoRes.error || !userInfoRes.response[0]) {
+      localStorage.removeItem(StorageKeys.VK_ID);
+      setInitLoading(false);
+      return;
+    }
+
+    const { id, last_name, first_name, photo_100 } = userInfoRes.response[0];
+    dispatch(setUser({ id, last_name, first_name, photo_100 }));
     dispatch(setInitLoading(false));
-    return;
-  }
 
-  // @TODO: delete this
-  await new Promise(resolve => setTimeout(() => resolve(true), 1000));
-  const userInfoRes = await VKLib.Api.getUserInfo(userId);
+    history.replace(redirectPath);
+  };
+}
 
-  if (userInfoRes.error || !userInfoRes.response[0]) {
+export function logoutAction(history: History): StoreThunk {
+  return async function(dispatch) {
+    const { setLoading, setUser } = userSlice.actions;
+
+    dispatch(setLoading(true));
+
+    // @TODO: delete this
+    await new Promise(resolve => setTimeout(() => resolve(true), 500));
+
+    await VKLib.Auth.logout();
+
     localStorage.removeItem(StorageKeys.VK_ID);
-    // @TODO: clean up
-    setInitLoading(false);
-    return;
-  }
+    dispatch(setUser(null));
+    dispatch(setLoading(false));
 
-  const { id, last_name, first_name, photo_100 } = userInfoRes.response[0];
-  dispatch(loginSuccess({ id, last_name, first_name, photo_100 }));
-  // @TODO: clean up
-  dispatch(setInitLoading(false));
-
-  history.replace(redirectPath);
-};
-
-export const logoutAction = (history: History): StoreThunk => async dispatch => {
-  const { setLoading, resetState, setInitLoading } = userSlice.actions;
-
-  dispatch(setLoading(true));
-
-  await VKLib.Auth.logout();
-  await new Promise(resolve => setTimeout(() => resolve(true), 500));
-
-  localStorage.removeItem(StorageKeys.VK_ID);
-  dispatch(resetState());
-  // @TODO: maybe init loading should be in another place
-  dispatch(setInitLoading(false));
-
-  history.replace("/login");
-};
+    history.replace("/login");
+  };
+}
