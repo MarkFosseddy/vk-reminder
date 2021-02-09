656850bf1a74b48161617575eabb1e3299d302bb
Fosseddy <MarkFosseddy@gmail.com>
2021-02-09 09:42:29 +0500

    add jest

---
+++ client/src/app.test.tsx
@@ -0,0 +1,12 @@
+// import React from "react";
+
+// import { render, screen } from "@testing-library/react";
+// import "@testing-library/jest-dom/extend-expect";
+
+// import { App } from "./app";
+
+describe("App component", () => {
+  it("should pass", () => {
+    expect(true).toBe(true);
+  });
+});
