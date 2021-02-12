11e21e7d83ca42c5ab03f8b8bed7382c6975c04e
Fosseddy <MarkFosseddy@gmail.com>
2021-02-12 21:34:05 +0500

    save changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,14 +10,22 @@ app.use(express.json());
 type Body = { id: string };
 
 app.post("/", async (req, res) => {
-  const { id } = req.body as Body;
-  if (!id) return res.status(400).json({ error: "No user id" });
+  try {
+    const { id } = req.body as Body;
+    if (!id) return res.status(400).json({ error: "No user id" });
 
-  const { data } = await axios.get(`https://api.vk.com/method/messages.isMessagesFromGroupAllowed?group_id=202435034&user_id=${id}&access_token=b76546debb5b7eaaf6ddf1c2839643aff090862728efe09743f2733033b5f40ad5e8d9d5d5753523bbda6&v=5.126`);
+    const { data } = await axios.get(`https://api.vk.com/method/messages.isMessagesFromGroupAllowed?group_id=202435034&user_id=${id}&access_token=b76546debb5b7eaaf6ddf1c2839643aff090862728efe09743f2733033b5f40ad5e8d9d5d5753523bbda6&v=5.126`);
+    if (data.error) {
+      return res.status(400).json({ error: data.error });
+    }
 
-  const is_allowed = !!data.response.is_allowed;
+    const is_allowed = !!data.response.is_allowed;
 
-  return res.status(200).json({ is_allowed });
+    return res.status(200).json({ is_allowed });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Server error" });
+  }
 });
 
 app.listen(3000, () => console.log("Server is listening on port 3000"));
